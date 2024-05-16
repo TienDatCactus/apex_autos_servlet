243138636457842f37d8ad27dee6a17b170251a6
Tien Dat <multivncraft@gmail.com>
2024-05-16 17:05:51 +0700

    change .html extensions to jsp , starts crafting the interiors

---
+++ src/java/Models/UserAccount.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Tiến_Đạt
+ */
+public class UserAccount {
+    String email;
+    String password;
+
+    public UserAccount(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
