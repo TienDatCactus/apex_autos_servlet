9dc8b5b0128fe23d5da17f2997ddf756773ff1f4
Tien Dat <multivncraft@gmail.com>
2024-05-16 19:47:17 +0700

    add user details class

---
+++ src/java/Models/UserDetails.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Tiến_Đạt
+ */
+public class UserDetails {
+
+    String fname;
+    String lname;
+
+    String dob;
+    String phone;
+
+    public UserDetails(String fname, String lname, String dob, String phone) {
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
+        this.phone = phone;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
