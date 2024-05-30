fe76bdeac5e7d1320c10d9a485935dca0c383871
Tien Dat <multivncraft@gmail.com>
2024-05-30 20:28:31 +0700

    Auto stash before merge of "main" and "origin/main"

---
+++ src/java/Controller/HomeControl.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controller;
+
+import DAO.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Tiến_Đạt
+ */
+@WebServlet(name="HomeControl", urlPatterns={"/home"})
+public class HomeControl extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HomeControl</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HomeControl at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  
+   @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        CarDAO dao = new CarDAO();
+         List<Car> listAllCars = dao.viewAllCar();
+         request.setAttribute("listAllCars", listAllCars);
+         request.getRequestDispatcher("front-end/index.jsp").forward(request, response);
+    } 
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+ 
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
