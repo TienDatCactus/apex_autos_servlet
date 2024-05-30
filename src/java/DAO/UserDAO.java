897f517ac1b45aaeb720860400782c1431fd8b45
Tien Dat <multivncraft@gmail.com>
2024-05-30 17:29:22 +0700

    errors pending

--- src/java/DAO/UserDAO.java
+++ src/java/DAO/UserDAO.java
@@ -13,10 +13,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.nio.charset.StandardCharsets;
 
-<<<<<<<<< Temporary merge branch 1
-
-=========
->>>>>>>>> Temporary merge branch 2
 /**
  *
  * @author Tiến_Đạt
@@ -121,8 +117,4 @@ public class UserDAO {
         }
     }
 
-<<<<<<<<< Temporary merge branch 1
-
-=========
->>>>>>>>> Temporary merge branch 2
 }
